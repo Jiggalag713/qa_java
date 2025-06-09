2fd4e079bee4b8f87ad95fae80b762324d809f27
Pavel Kiselev <pavel.kiselev2@lemanapro.ru>
2025-06-09 13:12:18 +0400

    Implement fifth sprint final project.

--- src/test/java/AlexTest.java
+++ src/test/java/AlexTest.java
@@ -1,2 +1,29 @@
+import com.example.Alex;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AlexTest {
+
+    Alex alex = new Alex();
+
+    public AlexTest() throws Exception {
+    }
+
+    @Test
+    public void getFriends() {
+        assertEquals(alex.getFriends(), List.of("Марти", "Глория", "Мелман"));
+    }
+
+    @Test
+    public void getPlaceOfLiving() {
+        assertEquals(alex.getPlaceOfLiving(), "Нью-Йоркский зоопарк");
+    }
+
+    @Test
+    public void getKittens() {
+        assertEquals(alex.getKittens(), 0);
+    }
 }
